d3d5cdcb3c6d913c11b97300adb7753af49f21fc
gammelinne <kylian.renault@viacesi.fr>
2022-02-03 12:15:40 +0100

    1.3.5

--- main.js
+++ main.js
@@ -253,7 +253,7 @@ client.on("interactionCreate", async (interaction) => {
 			le ping de kikibot est de ${client.ws.ping} ms (toujours pas plus que celui de la SNCF).\n`
 			)
 			.setFooter(
-				`Kikibot par ${interaction.guild.ownerId} Version 1.3.4`
+				`Kikibot par <@${interaction.guild.ownerId}> Version 1.3.2`
 			);
 		interaction.reply({ embeds: [pingembed] });
 	}
