c0531e41555e287c2dcecd023aec605b4ad2c9a3
gammelinne <kylian.renault@viacesi.fr>
2022-02-16 19:37:51 +0100

    1.3.6

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const { Client, Intents, MessageEmbed } = require("discord.js");
 const discordjs = require("discord.js");
 const fs = require("fs");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const {
+const {//
 	Permissions,
 	MessageActionRow,
 	MessageButton,
